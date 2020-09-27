e97b8da4f2527a70fe55b2cde334603cc95a4471
Fhexl <fakhrityhikmawan@gmail.com>
2020-09-27 16:32:58 +0700

    Update task-1.c

--- task-1.c
+++ task-1.c
@@ -2,6 +2,6 @@
 
 int main()
 {
-   printf("SELAMAT DATANG DI SEA CORP\a\n25 SEPTEMBER 2020\nCUACA CERAH\n");
+   printf("SELAMAT DATANG DI SEA CORP\a\n25 SEPTEMBER 2020\nCUACA CERAH");
    return 0;
 }
