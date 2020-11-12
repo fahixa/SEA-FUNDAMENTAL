d2971638cec4431c67a371954f356318ee2f7729
Fhexl <fakhrityhikmawan@gmail.com>
2020-11-12 18:49:31 +0700

    Create Week-2-Task-3.c

---
+++ Week-2-Task-3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main()
+{
+    int N[4][4];
+ 
+    for(int i = 1; i <= 3; i++){
+        for(int j = 1; j <= 3; j++){
+            printf("Baris %d Kolom %d : ",i,j);scanf("%d", &N[i][j]);
+        }
+    }
+    printf("\nMatriks : \n");
+    for(int i = 1; i <= 3; i++){
+            printf("[");
+        for(int j = 1; j <= 3; j++){
+            printf(" %d",N[i][j]);
+        }
+        printf(" ]\n");
+    }
+ 
+    return 0;
+}
+ 
